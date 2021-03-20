717c6136f8d9c0846007357ff08de4150216b50a
a-taghva <taghvali99@gmail.com>
2021-03-20 00:35:24 -0400

    searchProductHandler when search for a product

--- Public/javascript/dashboard.js
+++ Public/javascript/dashboard.js
@@ -4,8 +4,38 @@ async function getAllProducts() {
     saveProducts(products);
 };
 
+function searchProductHandler(e) {
+    e.preventDefault();
+    const productName = getProductName();
+
+    if(!productName) {
+        return alert('You need to enter an item!');
+    };
+
+    searchProduct(productName);
+};
+
+function getProductName() {
+    const productNameEl = document.querySelector("#p-name"); 
+    const productName = productNameEl.value.toLowerCase();
+
+    productNameEl.value = '';
+    return productName
+};
+
+async function searchProduct(pName) {
+    // the apiRoute is not correct!!!
+    const res = await fetch(`/api/products/${pName}`).then(res => res.json());
+    
+    if (res.ok) {
+        saveProducts(res);
+    }
+};
+
 function saveProducts(products) {
     localStorage.setItem('products', JSON.stringify(products));
 };
 
+
 getAllProducts();
+document.querySelector("#search-product").addEventListener('submit', searchProductHandler);
